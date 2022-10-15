beac9634a3b2cfc105328fff328ce0f82ec6ef76
eeeeagle <daniil.osockin@gmail.com>
2022-10-16 03:10:30 +0400

    Day 4 / Almost done

--- src/unnamed_pipe.hpp
+++ src/unnamed_pipe.hpp
@@ -7,13 +7,10 @@
 #include <wait.h>
 #define SLEEP_TIME 1
 
+typedef std::chrono::duration<double, std::milli> ms;
+
 namespace UP
 {
-    void on_exit()
-    {
-        std::cout << "sample text" << std::endl;
-    }
-
     void print_stat(const std::pair<bool, int>& result, const double& runtime)
     {
         std::cout << std::endl;
@@ -45,14 +42,14 @@ namespace UP
                 x = check(write(pipe_fd[1], &flag, sizeof(flag)));
             }
         }
+        usleep(1250000);
     }
 
     void guesser(int pipe_fd[], std::pair<bool, int>& result)
     {
         sleep(SLEEP_TIME);
         int n;
-        int r = check(read(pipe_fd[0], &n, sizeof(int)));
-        if (r > 0)
+        if (check(read(pipe_fd[0], &n, sizeof(int))))
         {
             bool flag = false;
             int i = 0;
@@ -60,53 +57,53 @@ namespace UP
             {
                 srandom(time(nullptr));
                 int value = 1 + (int)random() % n;
-                r = check(write(pipe_fd[1], &value, sizeof(value)));
+                check(write(pipe_fd[1], &value, sizeof(value)));
                 sleep(SLEEP_TIME);
 
-                r = check(read(pipe_fd[0], &flag, sizeof(bool)));
-                if (r > 0)
-                {
-                    std::cout << '[' << i + 1 << "]\t" << value << "\t" << (flag ? "true" : "false") << std::endl;
-                }
-                i++;
+                if (check(read(pipe_fd[0], &flag, sizeof(bool))))
+                    std::cout << '[' << i++ + 1 << "]\t" << value << "\t" << (flag ? "true" : "false") << std::endl;
             }
-            result = std::make_pair(flag, i + 1);
+            result = std::make_pair(flag, i);
         }
     }
 
     void start(const int n)
     {
         std::pair<bool, int> result;
-
         int fd[2];
         check(pipe(fd));
-        int p_id = check(fork());
+        pid_t p_id = check(fork());
 
         for(int i = 0; i < 10; i++)
         {
             auto start_time = std::chrono::high_resolution_clock::now();
-
             if (p_id)
             {
                 std::cout << "GAME [" << i + 1 << "]" << std::endl << std::endl;
 
                 if (i % 2)
+                {
                     riddler(fd, n);
+                }
                 else
+                {
                     guesser(fd, result);
-                int stat;
-                wait(&stat);
-
-                auto end_time = std::chrono::high_resolution_clock::now();
-                print_stat(result, std::chrono::duration<double, std::milli>(end_time - start_time).count() / 1000.0);
+                    auto end_time = std::chrono::high_resolution_clock::now();
+                    print_stat(result, std::chrono::duration<double>(end_time - start_time).count());
+                }
             }
             else
             {
-                atexit(on_exit);
                 if (i % 2)
+                {
                     guesser(fd, result);
+                    auto end_time = std::chrono::high_resolution_clock::now();
+                    print_stat(result, std::chrono::duration<double>(end_time - start_time).count());
+                }
                 else
+                {
                     riddler(fd, n);
+                }
             }
         }
         exit(EXIT_SUCCESS);
