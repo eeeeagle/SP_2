7eee93f43fcc9a44bace33cedfc5b6b21ef70f3e
eeeeagle <daniil.osockin@gmail.com>
2022-10-16 05:01:45 +0400

    Day 4 / Almost done

--- src/unnamed_pipe.hpp
+++ src/unnamed_pipe.hpp
@@ -5,30 +5,46 @@
 #include <chrono>
 #include <unistd.h>
 #include <wait.h>
-#define SLEEP_TIME 1
+#include <tuple>
 
-typedef std::chrono::duration<double, std::milli> ms;
+#define DELAY 10
 
 namespace UP
 {
-    void print_stat(const std::pair<bool, int>& result, const double& runtime)
+    void print_result(const std::pair<bool, int>& result, const double& runtime)
     {
-        std::cout << std::endl;
         std::cout << "Value was" << (result.first ? " " : "n't ") << "guessed" << std::endl;
         std::cout << "Number of attempts: " << result.second << std::endl;
-        std::cout << "Game time: " << runtime << " seconds" << std::endl;
+        std::cout << "Game time: " << (runtime / 1000.0) << " ms" << std::endl;
         std::cout << "__________________________________" << std::endl << std::endl;
+    }
+
+    void print_stat(const std::pair<std::pair<int, int>, double>& stats, const int n)
+    {
+        std::cout << "             Games: " << n << std::endl;
+
+        std::cout << "    Guessed values: " << stats.first.first << " (" << stats.first.first * 100 / n << "%)" << std::endl;
+
+        std::cout << "          Attempts: " << stats.first.second << std::endl;
+        std::cout << "  Average attempts: " << (int)(stats.first.second / n) << std::endl;
+
+        std::cout << " Average game time: " << (stats.second / 1000.0 / n) << " ms" << std::endl;
+
+        std::cout << "__________________________________" << std::endl;
     }
 
     void riddler(int pipe_fd[], const int n)
     {
-        srandom(time(nullptr));
+        struct timespec ts{};
+        clock_gettime(CLOCK_MONOTONIC, &ts);
+        srandom((time_t)ts.tv_nsec);
+
         int value = 1 + (int) random() % n;
-        sleep(SLEEP_TIME);
+        usleep(DELAY);
         std::cout << "Guessed value: " << value << std::endl;
 
         int x = check(write(pipe_fd[1], &n, sizeof(n)));
-        sleep(SLEEP_TIME);
+        usleep(DELAY);
 
         bool flag = false;
         while (!flag)
@@ -42,12 +58,12 @@ namespace UP
                 x = check(write(pipe_fd[1], &flag, sizeof(flag)));
             }
         }
-        usleep(1250000);
+        usleep(13);
     }
 
     void guesser(int pipe_fd[], std::pair<bool, int>& result)
     {
-        sleep(SLEEP_TIME);
+        usleep(DELAY);
         int n;
         if (check(read(pipe_fd[0], &n, sizeof(int))))
         {
@@ -55,58 +71,94 @@ namespace UP
             int i = 0;
             while (i < INT_MAX && !flag)
             {
-                srandom(time(nullptr));
+                struct timespec ts{};
+                clock_gettime(CLOCK_MONOTONIC, &ts);
+                srandom((time_t)ts.tv_nsec);
+
                 int value = 1 + (int)random() % n;
                 check(write(pipe_fd[1], &value, sizeof(value)));
-                sleep(SLEEP_TIME);
+                usleep(DELAY);
 
                 if (check(read(pipe_fd[0], &flag, sizeof(bool))))
                     std::cout << '[' << i++ + 1 << "]\t" << value << "\t" << (flag ? "true" : "false") << std::endl;
             }
+            std::cout << std::endl;
             result = std::make_pair(flag, i);
         }
     }
 
+    bool comp_1(const int i)
+    {
+        return (i % 2 == 0);
+    }
+
+    bool comp_2(const int i)
+    {
+        return !comp_1(i);
+    }
+
+    void shit(const int i, int fd[], const int n, std::pair<std::pair<int, int>, double>& stats, bool (*cmp)(const int))
+    {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        if (cmp(i))
+            riddler(fd, n);
+        else
+        {
+            std::pair<bool, int> result;
+            guesser(fd, result);
+
+            auto end_time = std::chrono::high_resolution_clock::now();
+            print_result(result, std::chrono::duration<double, std::micro>(end_time - start_time).count());
+
+            if(result.first)
+                stats.first.first++;
+            stats.first.second += result.second;
+            stats.second += std::chrono::duration<double, std::micro>(end_time - start_time).count();
+        }
+    }
+
+
     void start(const int n)
     {
-        std::pair<bool, int> result;
+        std::pair<std::pair<int, int>, double> stats {{0, 0}, 0.0};
+
         int fd[2];
         check(pipe(fd));
         pid_t p_id = check(fork());
 
         for(int i = 0; i < 10; i++)
         {
-            auto start_time = std::chrono::high_resolution_clock::now();
             if (p_id)
-            {
-                std::cout << "GAME [" << i + 1 << "]" << std::endl << std::endl;
-
-                if (i % 2)
-                {
-                    riddler(fd, n);
-                }
-                else
-                {
-                    guesser(fd, result);
-                    auto end_time = std::chrono::high_resolution_clock::now();
-                    print_stat(result, std::chrono::duration<double>(end_time - start_time).count());
-                }
-            }
+                shit(i, fd, n, stats, comp_1);
             else
-            {
-                if (i % 2)
-                {
-                    guesser(fd, result);
-                    auto end_time = std::chrono::high_resolution_clock::now();
-                    print_stat(result, std::chrono::duration<double>(end_time - start_time).count());
-                }
-                else
-                {
-                    riddler(fd, n);
-                }
-            }
+                shit(i, fd, n, stats, comp_2);
         }
-        exit(EXIT_SUCCESS);
+
+
+        if(p_id)
+        {
+            usleep(500);
+            std::pair<std::pair<int, int>, double> buffer;
+            if(check(read(fd[0], &buffer, sizeof(std::pair<std::pair<int, int>, double>))))
+            {
+                stats.first.first += buffer.first.first;
+                stats.first.second += buffer.first.second;
+                stats.second += buffer.second;
+            }
+            close(fd[0]);
+            close(fd[1]);
+
+            print_stat(stats, n);
+
+            waitpid(p_id, nullptr, 0);
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            check(write(fd[1], &stats, sizeof(std::pair<std::pair<int, int>, double>)));
+            usleep(DELAY);
+            exit(EXIT_SUCCESS);
+        }
     }
 }
 #endif
