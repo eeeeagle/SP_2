80dba27e5d8906d2f08bd7aee43d7ad534f0f230
eeeeagle <daniil.osockin@gmail.com>
2022-11-16 03:34:19 +0400

    Added setup default signal mask. Added process exist check. Added correct exit if other process exited. Renamed variables and functions for better readable. Improved process synchronization.

--- src/signal.hpp
+++ src/signal.hpp
@@ -21,158 +21,38 @@ namespace SIG
         signal_value    = signal_info->si_value.sival_int;
     }
 
-    bool exists(pid_t p, bool is_parent)
+    void default_sigmask()
+    {
+        struct sigaction    rt_action   {},
+                            usr1_action {},
+                            usr2_action {},
+                            quit_action {};
+
+        sigset_t set;
+        sigemptyset(&set);
+        sigfillset(&set);
+        sigprocmask(SIG_SETMASK, &set, nullptr);
+
+        rt_action.sa_sigaction = rt_sig_handler;
+        rt_action.sa_flags = SA_SIGINFO;
+        check(sigaction(SIGRTMAX, &rt_action, nullptr));
+
+        usr1_action.sa_handler = sig_handler;
+        check(sigaction(SIGUSR1, &usr1_action, nullptr));
+
+        usr2_action.sa_handler = sig_handler;
+        check(sigaction(SIGUSR2, &usr2_action, nullptr));
+
+        quit_action.sa_handler = sig_handler;
+        check(sigaction(SIGCHLD, &quit_action, nullptr));
+    }
+
+    bool is_exists(pid_t p, bool is_parent)
     {
         if (!is_parent)
             return (getpgid(p) >= 0);
         int stat;
         return waitpid(p, &stat, WNOHANG) == 0;
-    }
-
-    void riddler(pid_t p_id, const int n)
-    {
-        struct sigaction    rt_action {},
-                            usr1_action{},
-                            usr2_action{},
-                            quit_action{};
-        sigset_t set;
-        sigemptyset(&set);
-
-        rt_action.sa_sigaction = rt_sig_handler;
-        rt_action.sa_flags = SA_SIGINFO;
-        check(sigaction(SIGALRM, &rt_action, nullptr));
-
-        usr1_action.sa_handler = sig_handler;
-        check(sigaction(SIGUSR1, &usr1_action, nullptr));
-
-        usr2_action.sa_handler = sig_handler;
-        check(sigaction(SIGUSR2, &usr2_action, nullptr));
-
-        quit_action.sa_handler = sig_handler;
-        check(sigaction(SIGCHLD, &quit_action, nullptr));
-
-        struct timespec ts{};
-        clock_gettime(CLOCK_MONOTONIC, &ts);
-        srandom((time_t)ts.tv_nsec);
-
-        int value = 1 + (int) random() % n;
-        std::cout << "Guessed value: " << value << "\n\n";
-
-        check(sigqueue(p_id, SIGALRM, sigval{ n }));
-
-        bool flag = false;
-        while (!flag)
-        {
-            do
-            {
-                sigsuspend(&set);
-            } while(last_signal_id != SIGALRM);
-
-            if(signal_value == value)
-                flag = true;
-
-            check(kill(p_id, (flag ? SIGUSR1 : SIGUSR2)));
-        }
-    }
-
-    std::pair<bool, int> guesser(pid_t p_id, const bool is_parent)
-    /* Returns as pair the number of attempts and a Boolean value indicating whether the guessed number was guessed */
-    {
-        struct sigaction    rt_action {},
-                            usr1_action{},
-                            usr2_action{},
-                            quit_action{};
-        sigset_t set;
-        sigemptyset(&set);
-
-        rt_action.sa_sigaction = rt_sig_handler;
-        rt_action.sa_flags = SA_SIGINFO;
-        check(sigaction(SIGALRM, &rt_action, nullptr));
-
-        usr1_action.sa_handler = sig_handler;
-        check(sigaction(SIGUSR1, &usr1_action, nullptr));
-
-        usr2_action.sa_handler = sig_handler;
-        check(sigaction(SIGUSR2, &usr2_action, nullptr));
-
-        quit_action.sa_handler = sig_handler;
-        check(sigaction(SIGCHLD, &quit_action, nullptr));
-
-
-        if(!exists(p_id, is_parent))
-            exit(EXIT_FAILURE);
-
-        do
-        {
-            sigsuspend(&set);
-        } while (last_signal_id != SIGALRM);
-        const int n = signal_value;
-
-        struct timespec ts{};
-        clock_gettime(CLOCK_MONOTONIC, &ts);
-        srandom((time_t)ts.tv_nsec);
-
-        bool flag = false;
-        int i = 0;
-        std::vector<bool> was_guessed(n, false);
-        std::cout << std::setw(7) << "ATTEMPT" << std::setw(10) << "VALUE" << std::setw(12) << "FLAG\n";
-        while (i < INT_MAX && !flag)
-        {
-            int value;
-            do
-            {
-                value = 1 + (int)random() % n;
-            } while(was_guessed[value - 1]);
-            was_guessed[value - 1] = true;
-
-            if(!exists(p_id, is_parent))
-                exit(EXIT_FAILURE);
-
-            check(sigqueue(p_id, SIGALRM, sigval{ value }));
-            do
-            {
-                sigsuspend(&set);
-            } while (last_signal_id != SIGUSR1 && last_signal_id != SIGUSR2);
-
-            if (last_signal_id == SIGUSR1)
-                flag = true;
-
-            std::cout << std::setw(7) << i++ + 1 << std::setw(10) << value << std::setw(12) << (flag ? "true\n" : "false\n");
-        }
-        std::cout << '\n';
-        return std::make_pair(flag, i);
-    }
-
-    void player(const int i, pid_t p_id, const bool is_parent, const int n,
-                std::pair<std::pair<int, int>, double>& stats, bool (*cmp)(const int))
-    {
-        if (cmp(i))
-        {
-            raise(SIGSTOP);
-
-            std::cout << "GAME [" << i+1 << "]\n";
-            riddler(p_id, n);
-
-            raise(SIGSTOP);
-        }
-        else
-        {
-            std::cout << "__________________________________\n\n";
-            kill(p_id, SIGCONT);
-
-            auto start_time = HRC::now();
-            const std::pair<bool, int> result = guesser(p_id, is_parent);
-            auto end_time = HRC::now();
-
-            print_result(result, std::chrono::duration<double, std::micro>(end_time - start_time).count());
-
-            if(result.first)
-                stats.first.first++;
-            stats.first.second += result.second;
-            stats.second += Micro(end_time - start_time).count();
-
-            kill(p_id, SIGCONT);
-        }
     }
 
     void player_left()
@@ -183,98 +63,171 @@ namespace SIG
             exit(EXIT_SUCCESS);
     }
 
-    void on_end()
+    void player_riddler(const pid_t opponent, const int max_range, const bool is_parent)
     {
-        int stat;
-        wait(&stat);
-        if (last_signal_id == SIGCHLD)
-            exit(EXIT_SUCCESS);
+        static std::mt19937 gen(HRC::now().time_since_epoch().count());
+        std::uniform_int_distribution<> uid(1, max_range);
+
+        const int guessed_value = uid(gen);
+        std::cout << "Guessed value: " << guessed_value << "\n\n";
+
+        if(!is_exists(opponent, is_parent))
+            exit(EXIT_FAILURE);
+
+        check(sigqueue(opponent, SIGRTMAX, sigval{ max_range }));
+
+        sigset_t set;
+        sigemptyset(&set);
+        sigfillset(&set);
+        sigdelset(&set, SIGRTMAX);
+
+        bool is_guessed = false;
+        while (!is_guessed)
+        {
+            sigsuspend(&set);
+
+            if(signal_value == guessed_value)
+                is_guessed = true;
+
+            if(!is_exists(opponent, is_parent))
+                exit(EXIT_FAILURE);
+
+            check(kill(opponent, (is_guessed ? SIGUSR1 : SIGUSR2)));
+        }
     }
 
-    void start(const int n, const int count)
+    std::pair<bool, int> player_guesser(const pid_t opponent, const bool is_parent)
+    /* Returns as pair the number of attempts and a Boolean value indicating whether the guessed number was guessed */
     {
-        pid_t p_id = check(fork());
-
-        bool (*cmp)(const int);
-        bool is_parent;
-
-        if (p_id > 0)
         {
-            is_parent = true;
-            cmp = comp_1;
+            sigset_t set;
+            sigemptyset(&set);
+            sigfillset(&set);
+            sigdelset(&set, SIGRTMAX);
+            sigsuspend(&set);
+        }
+
+        const int max_range = signal_value;
+
+        std::vector<int> attempt = {};
+        for (int i = 1; i <= max_range; attempt.push_back(i++));
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::shuffle(attempt.begin(), attempt.end(), mt);
+
+        std::cout << std::setw(7)  << "ATTEMPT"
+                  << std::setw(10) << "VALUE"
+                  << std::setw(12) << "FLAG" << std::endl;
+
+        sigset_t set;
+        sigemptyset(&set);
+        sigfillset(&set);
+        sigdelset(&set, SIGUSR1);
+        sigdelset(&set, SIGUSR2);
+
+        int try_count = 0;
+        bool is_guessed = false;
+        while (!attempt.empty() && try_count < INT_MAX && !is_guessed)
+        {
+            int value = attempt.back();
+            attempt.pop_back();
+
+            if(!is_exists(opponent, is_parent))
+                exit(EXIT_FAILURE);
+            check(sigqueue(opponent, SIGRTMAX, sigval{value}));
+
+            do
+            {
+                sigsuspend(&set);
+
+                if (last_signal_id == SIGUSR1)
+                    is_guessed = true;
+            } while (last_signal_id != SIGUSR1 && last_signal_id != SIGUSR2);
+            std::cout << std::setw(7) << ++try_count
+                      << std::setw(10) << value
+                      << std::setw(12) << (is_guessed ? "true" : "false") << std::endl;
+        }
+        std::cout << std::endl;
+        return std::make_pair(is_guessed, try_count);
+    }
+
+    void role_select(const int game_count, const pid_t opponent, const int max_range,
+                     OverallStat& stats, bool (*const cmp)(const int), const bool is_parent)
+    {
+        if (cmp(game_count))
+        {
+            usleep(500);
+            std::cout << "__________________________________\n\n";
+            std::cout << "GAME [" << game_count + 1 << "]\n";
+            player_riddler(opponent, max_range, is_parent);
         }
         else
         {
-            is_parent = false;
-            cmp = comp_2;
+            auto start_time = HRC::now();
+            const std::pair<bool, int> result = player_guesser(opponent, is_parent);
+            auto end_time = HRC::now();
+
+            print_result(result, Micro(end_time - start_time).count());
+
+            if(result.first)
+                stats.guessed++;
+            stats.attempts += result.second;
+            stats.total_time += Micro(end_time - start_time).count();
         }
+    }
+
+    void start(const int max_range, const int max_game_count)
+    {
+        default_sigmask();
+        pid_t p_id = check(fork());
+
+        const bool is_parent = p_id != 0;
+        bool (* const cmp)(const int) = is_parent ? comp_1 : comp_2;
 
         if (atexit(player_left))
         {
-            fprintf(stderr, "Failed to register function 1");
-            _exit(EXIT_FAILURE);
+            fprintf(stderr, "Failed to register role_select left function");
+            exit(EXIT_FAILURE);
         }
 
-        std::pair<std::pair<int, int>, double> stats {{0, 0}, 0.0};
-        for(int i = 0; i < count; i++)
-            player(i, p_id ? p_id : getppid(), is_parent, n, stats, cmp);
+        OverallStat stats {0, 0, 0.0};
+        for(int game_count = 0; game_count < max_game_count; game_count++)
+            role_select(game_count, p_id ? p_id : getppid(), max_range, stats, cmp, is_parent);
 
         if(p_id)
         {
-            struct sigaction rt_action {};
+            struct sigaction    rt_action   {},
+                                quit_action {};
             sigset_t set;
             sigemptyset(&set);
 
             rt_action.sa_sigaction = rt_sig_handler;
             rt_action.sa_flags = SA_SIGINFO;
-            check(sigaction(SIGALRM, &rt_action, nullptr));
+            check(sigaction(SIGRTMAX, &rt_action, nullptr));
 
-            if(!exists(p_id, is_parent))
-                exit(EXIT_FAILURE);
+            quit_action.sa_handler = sig_handler;
+            check(sigaction(SIGCHLD, &quit_action, nullptr));
 
-            do
-            {
-                sigsuspend(&set);
-            } while(last_signal_id != SIGALRM);
-            stats.first.first += signal_value;
+            sigsuspend(&set);
+            stats.guessed += signal_value;
 
-            if(!exists(p_id, is_parent))
-                exit(EXIT_FAILURE);
+            sigsuspend(&set);
+            stats.attempts += signal_value;
 
-            do
-            {
-                sigsuspend(&set);
-
-            } while(last_signal_id != SIGALRM);
-            stats.first.second += signal_value;
-
-            if(!exists(p_id, is_parent))
-                exit(EXIT_FAILURE);
-
-            do
-            {
-                sigsuspend(&set);
-
-            } while(last_signal_id != SIGALRM);
-            stats.second += signal_value;
+            sigsuspend(&set);
+            stats.total_time += signal_value;
 
             std::cout << "__________________________________\n\n";
-            print_stat(stats, count);
+            print_stat(stats, max_game_count);
             std::cout << "__________________________________\n\n";
 
-            if (atexit(on_end))
-            {
-                fprintf(stderr, "Failed to register function 1");
-                _exit(EXIT_FAILURE);
-            }
             exit(EXIT_SUCCESS);
         }
         else
         {
-            check(sigqueue(getppid(), SIGALRM, sigval{stats.first.first}));
-            check(sigqueue(getppid(), SIGALRM, sigval{stats.first.second}));
-            check(sigqueue(getppid(), SIGALRM, sigval{(int)(stats.second)}));
-
+            check(sigqueue(getppid(), SIGRTMAX, sigval{stats.guessed}));
+            check(sigqueue(getppid(), SIGRTMAX, sigval{stats.attempts}));
+            check(sigqueue(getppid(), SIGRTMAX, sigval{(int)stats.total_time}));
             exit(EXIT_SUCCESS);
         }
     }
