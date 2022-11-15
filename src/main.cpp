e50f47017b16bda086ceab745ea01bb39a137e3d
eeeeagle <daniil.osockin@gmail.com>
2022-11-16 03:30:15 +0400

    Changed argument check and parsing.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include "signal.hpp"
 #include "unnamed_pipe.hpp"
 #include "message_queue.hpp"
-#include <string>
+#include <getopt.h>
+#include <cstring>
 
 int get_int(const std::string& argv)
 {
-    int value;
     try
     {
-        int i {std::stoi(argv)};
-        value = i;
+        return std::stoi(argv);
     }
     catch(std::invalid_argument const& ex)
     {
@@ -19,97 +18,120 @@ int get_int(const std::string& argv)
     }
     catch(std::out_of_range const& ex)
     {
-        throw std::out_of_range("OUT OF RANGE: N = " + std::string(argv));
+        std::cout   << "OUT OF RANGE: N = " + std::string(argv) << '\n'
+                    << "    Value must be in range [INT_MIN; INT_MAX]\n\n";
+        exit(EXIT_FAILURE);
     }
-    return value;
 }
 
 void help()
 {
     std::cout   << "Use those arguments to launch program:\n"
                 << "    [mode]\n"
-                << "    -n [value]         (optional)\n"
-                << "    -c [count]         (optional)\n\n"
+                << "    --range (-n) [value]         (optional)\n"
+                << "    --count (-c) [count]         (optional)\n\n"
                 << "mode\n"
                 << "    \"Guess the value\" mode:\n"
-                << "    [-sig] Signal\n"
-                << "    [ -up] Unnamed pipe\n"
-                << "    [ -mq] Message queue\n\n"
+                << "    [--sig] Signal\n"
+                << "    [ --up] Unnamed pipe\n"
+                << "    [ --mq] Message queue\n\n"
                 << "value\n"
                 << "    Set maximum value to guess in the game\n"
                 << "    Range for the value: [2, INT_MAX]\n\n"
                 << "count\n"
                 << "    Set number of games\n"
-                << "    Minimum: 10\n\n"
+                << "    Range for the value: [10, INT_MAX]\n\n"
                 << "EXAMPLE:\n"
-                << "    ./SP_2 -sig -n 1000 -c 50\n";
+                << "    ./SP_2 --sig --range 100 -c 50\n";
 }
 
 int main(int argc, char** argv)
 {
-    if(argc != 2 && argc != 4 && argc != 6)
+    if(argc == 2 && (strcmp(argv[1],"--help") == 0 || strcmp(argv[1], "-h") == 0))
     {
         help();
         exit(EXIT_FAILURE);
     }
 
-    int n = 1;
-    int count = 10;
-    try
+    int range = 1,
+        count = 10,
+         mode = -1;
+
+    const option options[] = {{  "sig",       no_argument, nullptr, 's'},
+                              {   "up",       no_argument, nullptr, 'p'},
+                              {   "mq",       no_argument, nullptr, 'q'},
+                              {"range", required_argument, nullptr, 'n'},
+                              {"count", required_argument, nullptr, 'c'},
+                              {nullptr,       no_argument, nullptr,  0 }};
+
+    bool range_flag = false;
+    int i;
+    while((i = getopt_long(argc, argv, "n:c:", options, nullptr)) != -1)
     {
-        if (std::string(argv[1]) != "-sig" &&
-            std::string(argv[1]) != "-up" &&
-            std::string(argv[1]) != "-mq")
-            throw std::invalid_argument("INVALID MODE: " + std::string(argv[1]));
-
-        if (argc >= 4 && (std::string(argv[2]) != "-n" && std::string(argv[2]) != "-c"))
-            throw std::invalid_argument("INVALID MODE: " + std::string(argv[2]));
-
-        if (argc == 6 && (std::string(argv[4]) != "-n" && std::string(argv[4]) != "-c"))
-            throw std::invalid_argument("INVALID MODE: " + std::string(argv[4]));
-
-        if ((argc >= 4 && std::string(argv[2]) == "-n") || (argc == 6 && std::string(argv[4]) == "-n"))
-            n = get_int(std::string(argv[2]) == "-n" ? argv[3] : argv[5]);
-        else
+        switch(i)
         {
-            std::cout << "Enter N: ";
-            std::cin >> n;
+            case 's':
+                mode = 's';
+                break;
+            case 'p':
+                mode = 'p';
+                break;
+            case 'q':
+                mode = 'q';
+                break;
+            case 'n':
+                range = get_int(optarg);
+                range_flag = true;
+                break;
+            case 'c':
+                count = get_int(optarg);
+                break;
+            default:
+                std::cout << "Use [--help] or [-h] argument to get launch information\n";
+                std::cout << optarg;
+                exit(EXIT_FAILURE);
         }
-        if(n < 2)
-            throw std::out_of_range("OUT OF RANGE: N = " + std::to_string(n));
-
-        if ((argc >= 4 && std::string(argv[2]) == "-c") || (argc == 6 && std::string(argv[4]) == "-c"))
-        {
-            count = get_int(std::string(argv[2]) == "-c" ? argv[3] : argv[5]);
-            if (count < 10)
-                throw std::out_of_range("OUT OF RANGE: count = " + std::to_string(count));
-        }
-
     }
-    catch(std::invalid_argument const& ex)
-    {
-        std::cout   << ex.what() << "\n\n"
-                    << "Use those arguments to launch program:\n"
-                    << "    [-sig] Signal\n"
-                    << "    [ -up] Unnamed pipe\n"
-                    << "    [ -mq] Message queue\n\n";
-        exit(EXIT_FAILURE);
-    }
-    catch(std::out_of_range const& ex)
-    {
-        std::cout   << ex.what() << "\n\n"
-                    << "Value must be in range:\n"
-                    << "        N = [2, INT_MAX]\n"
-                    << "    count = [10, INT_MAX]\n\n";
 
+    if(count < 10)
+    {
+        std::cout   << "count = " << count << '\n'
+                    << "Value must be in range [10, INT_MAX]\n";
         exit(EXIT_FAILURE);
     }
 
-    system("clear");
-    if (std::string(argv[1]) == "-sig")
-        SIG::start(n, count);
-    else if (std::string(argv[1]) == "-up")
-        UP::start(n, count);
-    else if (std::string(argv[1]) == "-mq")
-        MQ::start(n, count);
+    if(!range_flag)
+    {
+        std::string buf;
+        std::cout << "Enter N: ";
+        std::cin >> buf;
+        range = get_int(buf);
+    }
+
+    if(range < 2)
+    {
+        std::cout   << "range = " << range << '\n'
+                    << "Value must be in range [2, INT_MAX]\n";
+        exit(EXIT_FAILURE);
+    }
+
+    switch(mode)
+    {
+        case 's':
+            SIG::start(range, count);
+            break;
+        case 'p':
+            UP::start(range, count);
+            break;
+        case 'q':
+            MQ::start(range, count);
+            break;
+        default:
+            std::cout   << "Use one of those arguments to launch game:\n"
+                        << "    [--sig] Signal\n"
+                        << "    [ --up] Unnamed pipe\n"
+                        << "    [ --mq] Message queue\n\n";
+            exit(EXIT_FAILURE);
+    }
+    return 0;
 }
